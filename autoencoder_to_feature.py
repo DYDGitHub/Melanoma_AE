95c2afa622b8fcbb376b3d538cb2657207c044e8
DYDGitHub <48060902+DYDGitHub@users.noreply.github.com>
2021-03-29 16:40:36 -0500

    Add files via upload

---
+++ autoencoder_to_feature.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import torch
+import sys
+from torch import nn
+from torch.autograd import Variable
+from sklearn import preprocessing
+
+
+parList = sys.argv
+indexSample = int(parList[1])
+indexScore = int(parList[2])
+indexGroup = int(parList[3])
+learning_rate = float(parList[4])
+
+
+if indexGroup == 1:
+    num_hidden1 = 1000
+    num_hidden2 = 100
+    num_feature = 20
+else:
+    num_hidden1 = 400
+    num_hidden2 = 100
+    num_feature = 20
+
+sampleType = ["metastatic_", "All_Symbol_"]
+scoreType = ["lymphocyte_", "purity_"]
+groupName = ["high", "middle", "low"]
+combineName = sampleType[indexSample] + scoreType[indexScore] + groupName[indexGroup]
+
+inputName = "SKCM_RNAseqv3_" + combineName + ".csv"
+outputName = "autoencoder_" + sampleType[0] + scoreType[indexScore] + groupName[indexGroup] + "_" + str(num_feature) + "_" + str(learning_rate) + ".pth"
+featureName = "AutoEncoder_features_" + combineName + "_" + str(num_feature) + "_" + str(learning_rate) + ".txt"
+
+
+rna_df = pd.read_csv(inputName, sep="\t")
+rna = np.array(rna_df)
+
+num_input = len(rna)
+
+if np.max(rna) > 10000:
+    rna = np.log10(rna + 1)
+
+rna = rna.T
+rna = preprocessing.scale(rna)
+
+print(sum(rna[:, 0]))
+print(rna.shape)
+print(len(rna))
+print(combineName)
+print("hidden layer: " + str(num_hidden1) + ',' + str(num_hidden2))
+print(featureName)
+print(learning_rate)
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(num_input, num_hidden1),
+            nn.ReLU(True),
+            nn.Linear(num_hidden1, num_hidden2),
+            nn.ReLU(True),
+            nn.Linear(num_hidden2, num_feature))
+
+        self.decoder = nn.Sequential(
+            nn.Linear(num_feature, num_hidden2),
+            nn.ReLU(True),
+            nn.Linear(num_hidden2, num_hidden1),
+            nn.ReLU(True),
+            nn.Linear(num_hidden1, num_input),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+model = AutoEncoder()
+model.load_state_dict(torch.load(outputName, map_location='cpu'))
+
+featureList = []
+for sample in rna:
+    sample = Variable(torch.FloatTensor(sample))
+    features = model.encoder(sample)
+    features = features.detach().numpy()
+    featureList.append(features)
+
+
+np.savetxt(featureName, featureList, delimiter='\t')
